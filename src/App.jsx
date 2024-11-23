877a62e2f01521a963dd1caf60e2542789b323b4
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-24 01:08:51 +0600

    App JSX Component Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import { useEffect } from 'react';
 
+
+
 function App() {
   const [users, setUsers] = useState([]);
 
@@ -11,12 +13,38 @@ function App() {
       .then(data => setUsers(data))
   }, [])
 
+  const handleAddUser = e => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = {name, email};
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+  }
+
   return (
-    <>
-        
+    <>      
       <h1>Users Management System</h1>
       <h2>Numbers of Users: {users.length}</h2>
-      
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' id='' />
+        <br />
+        <input type="email" name='email' id='' />
+        <br /><br />
+        <input type="submit" value="Add User" />       
+      </form>
+        <div>
+          {
+            users.map(user => <p key={user.id}>{user.id}:- {user.name}, {user.email}</p>)
+          }
+        </div>
     </>
   )
 }
