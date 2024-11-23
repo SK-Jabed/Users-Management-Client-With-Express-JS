066d1491b3faa5805cd8365318ed5a0a0ee61c7b
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-23 22:59:20 +0600

    Users Data Fetched by useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
 
   return (
     <>
         
       <h1>Users Management System</h1>
+      <h2>Numbers of Users: {users.length}</h2>
       
     </>
   )
